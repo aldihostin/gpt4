ecc4ae0a195dfe2079925cccded27d2835cc9705
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 02:10:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', searchAnswer);
+});
+
 async function searchAnswer() {
   const question = document.getElementById('question').value;
   const answerDiv = document.getElementById('answer');
   
   try {
-    const response = await fetch(`/gpt4?text=${encodeURIComponent(question)}`);
+    const response = await fetch(`/openai?text=${encodeURIComponent(question)}`);
     const json = await response.json();
     const answer = json.result;
     
+    if (json.status) {
+    
     answerDiv.innerText = answer;
+    } else {
+    answerDiv.innerText = 'tidak dapat menyelesaikan';
+    }
   } catch (error) {
     console.error(error);
-    answerDiv.innerText = 'Terjadi kesalahan saat mencari jawaban.';
+    answerDiv.innerText = 'Terjadi kesalahan .';
   }
 }
