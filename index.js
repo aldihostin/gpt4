f301acd64cc037c73593da3fa629b71b9a870b51
Aldiansyah putra <144991793+aldihostin@users.noreply.github.com>
2024-05-07 07:41:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-
-app.get('/openai', async (req, res) => {
-  const { text } = req.query;
-  
-    const response = await fetch(`https://aemt.me/openai?text=${encodeURIComponent(text)}`);
-    const json = await response.json();
-    res.json(json)
-});
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
-});
